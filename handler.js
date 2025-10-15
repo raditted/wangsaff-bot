48f5f151e1907e329b248d9191a0ed6b9a9d21ba
raditted <radithya.syabil@gmail.com>
2025-10-15 15:37:17 +0700

    refactor: streamline sticker command handling and remove unused code

--- handler.js
+++ handler.js
@@ -1,9 +1,6 @@
-import { Sticker, StickerTypes } from "wa-sticker-formatter"
 import "dotenv/config"
-import { PREFIX } from "./config.js"
-import { downloadMediaMessage } from "@whiskeysockets/baileys"
-import axios from "axios"
-import { createAdReplyContext } from "./utils/contextInfo.js"
+import { handlerSticker } from "./utils/stickerHandler.js"
+import { PREFIX, cooldowns, cdDelay } from "./config.js"
 
 export const messagesHandler = async (messages, sock) => {
     const msg = messages.messages?.[0] || messages[0]
@@ -20,101 +17,24 @@ export const messagesHandler = async (messages, sock) => {
 
     if (!text.startsWith(PREFIX)) return
 
+    const isCD = await cdDelay(sender, sock, msg)
+    if (isCD) {
+        return; 
+    }
+
     const commandBody = text.slice(PREFIX.length).trim()
     const [command] = commandBody.split(/\s+/)
+
+    const now = Date.now();
 
     switch (command.toLowerCase()) {
         case "stiker":
         case "sticker":
         case "s": {
-            await sock.sendMessage(sender, { react: { text: "⏳", key: msg.key } })
-
-            const isImage =
-                msg.message.imageMessage ||
-                msg.message.extendedTextMessage?.contextInfo?.quotedMessage
-                ?.imageMessage
-                
-            let thumbBuffer = null
-
-            try {
-                const thumbnailUrl = process.env.THUMBNAIL_URL
-                const response = await axios.get(thumbnailUrl, {
-                    responseType: "arraybuffer",
-                })
-                thumbBuffer = Buffer.from(response.data, "binary")
-            } catch (error) {
-                console.error("❌ error get thumbnail:", error)
-            }
-            
-            const context = createAdReplyContext(thumbBuffer)
-
-            if (!isImage) {
-                try {
-                    await sock.sendMessage(sender, { react: { text: "❌", key: msg.key } })
-                    await sock.sendMessage(
-                        sender,
-                        {
-                            text: `> *Kirimkeun gambar sia jeung caption atawa reply gambar anu antos di send ka WA ku panitah ieu:*\n\`\`\`\n.sticker\n.stiker\n.s\n\`\`\``,
-                            contextInfo: context
-                        },
-                        { quoted: msg }
-                    )
-                } catch (err) {
-                    console.error("❌ error:", err)
-                }
-                return
-            }
-
-            try {
-                await sock.sendMessage(
-                    sender,
-                    {
-                    text: "Keur proses yeuh blog...",
-                    contextInfo: context,
-                    },
-                    { quoted: msg }
-                )
-                const targetMessage = msg.message.imageMessage
-                    ? msg
-                    : msg.message.extendedTextMessage?.contextInfo?.quotedMessage
-
-                const buffer = await downloadMediaMessage(
-                    targetMessage,
-                    "buffer",
-                    {},
-                    { reuploadRequest: sock.updateMediaMessage }
-                )
-
-                const sticker = new Sticker(buffer, {
-                    pack: "Saint-Chamond",
-                    author: "@raditted",
-                    type: StickerTypes.FULL,
-                    quality: 80,
-                })
-
-                const stickerBuffer = await sticker.build()
-                await sock.sendMessage(
-                    sender,
-                    { 
-                        sticker: stickerBuffer,
-                    },
-                    { quoted: msg }
-                )
-                await sock.readMessages([msg.key])
-                await sock.sendMessage(sender, { react: { text: "✅", key: msg.key } })
-
-            } catch (error) {
-                console.error("❌ error make sticker:", error)
-                await sock.sendMessage(
-                    sender, 
-                    { 
-                        text: "Botna moal baleg, gagal jieun stiker.",
-                        contextInfo: context 
-                    }, 
-                    { quoted: msg }
-                )
-            }
+            cooldowns.set(sender, now)
+            handlerSticker(msg, sock, sender)
             break
         }
     }
 }
+
