ba7d5a51a39ee104d3e97a540f80bf5aca15cf37
raditted <radithya.syabil@gmail.com>
2026-07-31 16:40:50 +0700

    fix: update cooldown management to use cdKey for consistency

--- config.js
+++ config.js
@@ -4,25 +4,26 @@ export const cooldowns = new Map()
 const cdSec = 60
 const delayMs = 1500 // 1.5 s
 
-export const cdDelay = async (userJid, sender, sock, msg) => {
+export const cdDelay = async (userJid, sender, sock, msg, keySuffix = '', customDuration = null) => {
+    const cdKey = userJid + keySuffix
     const now = Date.now()
-    if (cooldowns.has(userJid)) {
-        const cdData = cooldowns.get(userJid)
+    if (cooldowns.has(cdKey)) {
+        const cdData = cooldowns.get(cdKey)
         const lastCommandTime = cdData.time || cdData
         const hasWarned = cdData.warned || false
         
-        const duration = cdData.duration || 60
+        const duration = customDuration || cdData.duration || 60
         
         const timePassed = (now - lastCommandTime) / 1000
         
         if (timePassed < duration) {
             if (!hasWarned) {
                 const timeLeft = Math.ceil(duration - timePassed)
-                const textMsg = duration === 60 
+                const textMsg = duration >= 60 
                     ? `CD Stiker ${timeLeft} detik lagi` 
                     : `CD kirim petunjuk ${timeLeft} detik lagi`
                 await sock.sendMessage(sender, { text: textMsg }, { quoted: msg })
-                cooldowns.set(userJid, { time: lastCommandTime, duration, warned: true })
+                cooldowns.set(cdKey, { time: lastCommandTime, duration, warned: true })
             }
             return true // stop exec
         }
