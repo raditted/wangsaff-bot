210cc005c533f33aa3e3cd0f4bf680452701b1aa
raditted <radithya.syabil@gmail.com>
2026-07-31 16:25:32 +0700

    fix: update cooldown management to use userJid instead of sender

--- config.js
+++ config.js
@@ -4,20 +4,25 @@ export const cooldowns = new Map()
 const cdSec = 60
 const delayMs = 1500 // 1.5 s
 
-export const cdDelay = async (sender, sock, msg) => {
+export const cdDelay = async (userJid, sender, sock, msg) => {
     const now = Date.now()
-    if (cooldowns.has(sender)) {
-        const cdData = cooldowns.get(sender)
+    if (cooldowns.has(userJid)) {
+        const cdData = cooldowns.get(userJid)
         const lastCommandTime = cdData.time || cdData
         const hasWarned = cdData.warned || false
         
+        const duration = cdData.duration || 60
+        
         const timePassed = (now - lastCommandTime) / 1000
         
-        if (timePassed < cdSec) {
+        if (timePassed < duration) {
             if (!hasWarned) {
-                const timeLeft = Math.ceil(cdSec - timePassed)
-                await sock.sendMessage(sender, { text: `Keheula any... CD ${timeLeft} detik deui. (CD 1 menit)` }, { quoted: msg })
-                cooldowns.set(sender, { time: lastCommandTime, warned: true })
+                const timeLeft = Math.ceil(duration - timePassed)
+                const textMsg = duration === 60 
+                    ? `CD Stiker ${timeLeft} detik lagi` 
+                    : `CD kirim petunjuk ${timeLeft} detik lagi`
+                await sock.sendMessage(sender, { text: textMsg }, { quoted: msg })
+                cooldowns.set(userJid, { time: lastCommandTime, duration, warned: true })
             }
             return true // stop exec
         }
